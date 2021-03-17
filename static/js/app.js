fdecc53a91ecf34d3cbe7362d18d5457320eb993
null-architect <stry.bout.eyes@gmail.com>
2021-03-17 11:48:21 +0900

    Update ajax with better animation

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,20 @@
 // Ajax with animation for card-today
 $(document).ready(
-  $("#card-hide-today").on( "click",
+  $("#card-hide-today").on("click",
     function () {
-      $(this).css('opacity', '0');
-      $("#card-body").css('max-height', '1000px');
-      $.get("/prediction/today", function(data, status){
-        $("#card-text-today").text(data);
+      $(".card-hide-wrap").hide();
+      $.ajax({
+        url: "/prediction/today",
+        beforeSend: function () {
+          $('#loader').show();
+        },
+        complete: function () {
+          $("#card-hide-today").css('opacity', '0');
+        },
+        success: function (data) {
+          $("#card-body").css('max-height', '600px');
+          $("#card-text-today").text(data);
+        }
       });
     }
   )
